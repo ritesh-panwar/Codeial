5d48952721edab0a3e1dd2175b7eacf18f7b7189
Ritesh Panwar <riteshpanwar110@gmail.com>
2021-07-03 17:13:40 +0530

    Multiple Routes and controllers

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../controllers/users_controller');
 
 router.get('/profile', userController.profile);
+router.use('/posts', userController.posts);
 
 module.exports = router;
