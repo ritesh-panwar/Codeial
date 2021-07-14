a9e32822b9c0c529dc740bde2a384822e2c608ca
Ritesh Panwar <riteshpanwar110@gmail.com>
2021-07-15 00:12:43 +0530

    Setting Up Signin and Signup

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,3 +7,19 @@ module.exports.profile = function(req, res){
 module.exports.posts = function(req, res){
     res.end('<h1>All Your Posts are here!</h1>');
 }
+
+//render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    });
+}
+
+
+//render the sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
+    });
+}
+
