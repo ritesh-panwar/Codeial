6fef01b92c1f3f399a6fe471bf9b18440b142ff4
Ritesh Panwar <riteshpanwar110@gmail.com>
2021-07-16 00:46:28 +0530

    Setting-up PassportJS for authentication-2

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 const userController = require('../controllers/users_controller');
 
-router.get('/profile', userController.profile);
+router.get('/profile', passport.checkAuthentication, userController.profile);
 router.use('/posts', userController.posts);
 
 router.get('/sign-up', userController.signUp);
