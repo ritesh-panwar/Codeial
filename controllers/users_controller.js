106573966ada63edc46d4941fdf7334b35be77ed
Ritesh Panwar <riteshpanwar110@gmail.com>
2021-07-15 16:04:27 +0530

    Manual Sign-in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,19 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: "User Profile"
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if(user){
+                return res.render('user_profile', {
+                    title: "User Profile",
+                    user: user
+                });
+            }
+            return res.redirect('/users/sign-in'); 
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 }
 
 module.exports.posts = function(req, res){
@@ -55,5 +65,28 @@ module.exports.create = function(req, res){
 
 //Sign In and create a session for the user
 module.exports.createSession = function(req, res){
-    //TODO Later
+    //steps to authenticate
+    //find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('Error in finding the user in signing-in');
+            return;
+        }
+        if(user){
+            //handle password which don't match
+            if(user.password != req.body.password){
+                console.log("Incorrect Password!");
+                return res.redirect('back');
+            }
+            //handle session creation
+            res.cookie('user_id', user.id);
+            console.log(user.id);
+            return res.redirect('/users/profile');
+        }
+        else{
+            console.log("User Not Found!");
+            return res.redirect('back');
+        }
+    });
+
 }
