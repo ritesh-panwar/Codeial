90959d760b4c27aadc8d8d18877a2ea8eda6c96e
Ritesh Panwar <riteshpanwar110@gmail.com>
2021-07-30 00:14:27 +0530

    User Profile Links Added

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 const userController = require('../controllers/users_controller');
 
-router.get('/profile', passport.checkAuthentication, userController.profile);
+router.get('/profile/:id', passport.checkAuthentication, userController.profile);
 router.use('/posts', userController.posts);
 
 router.get('/sign-up', userController.signUp);
