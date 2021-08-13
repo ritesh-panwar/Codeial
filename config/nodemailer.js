1758c968c663691a649bf6cee82cf3443a2a2952
ritesh-panwar <85789718+ritesh-panwar@users.noreply.github.com>
2021-08-13 17:51:40 +0530

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -9,7 +9,7 @@ let transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: 'codeialcn@gmail.com',
-        pass: 'CodeialSocial'
+        pass: '*********'
     }
 });
 
